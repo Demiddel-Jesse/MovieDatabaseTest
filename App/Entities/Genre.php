16c5065e4dc49441db866eef3e2372d0e4817c83
Demiddel-Jesse <jesse.demiddel19@gmail.com>
2024-04-29 12:53:22 +0200

    created basic genre entity

---
+++ App/Entities/Genre.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entities;
+
+class Genre
+{
+  private int $id;
+  private string $name;
+
+  public function __construct(int $id, string $name)
+  {
+    $this->id = $id;
+    $this->name = $name;
+  }
+}
