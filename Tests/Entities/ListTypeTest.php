8778f567da625047359222d4458536305685f193
Demiddel-Jesse <jesse.demiddel19@gmail.com>
2024-04-29 13:03:13 +0200

    copied genre entity (basicly the same)

---
+++ Tests/Entities/ListTypeTest.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Entities\Genre;
+use PHPUnit\Framework\TestCase;
+
+class ListTypeTest extends TestCase
+{
+  private $data;
+  private $object;
+
+  public function setUp(): void
+  {
+    $this->data = ['id' => 1, 'name' => 'nog te bekijken'];
+    $this->object = new Genre($this->data['id'], $this->data['name']);
+  }
+
+  public function test_getId()
+  {
+    $this->assertEquals($this->data['id'], $this->object->getId());
+  }
+
+  public function test_getName()
+  {
+    $this->assertEquals($this->data['name'], $this->object->getName());
+  }
+}
