399b948b6558ce933643e29fab21f3d737054ab1
Demiddel-Jesse <jesse.demiddel19@gmail.com>
2024-04-29 13:06:21 +0200

    adjusted object

--- Tests/Entities/ListTypeTest.php
+++ Tests/Entities/ListTypeTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-use App\Entities\Genre;
+use App\Entities\ListType;
 use PHPUnit\Framework\TestCase;
 
 class ListTypeTest extends TestCase
@@ -13,7 +13,7 @@ class ListTypeTest extends TestCase
   public function setUp(): void
   {
     $this->data = ['id' => 1, 'name' => 'nog te bekijken'];
-    $this->object = new Genre($this->data['id'], $this->data['name']);
+    $this->object = new ListType($this->data['id'], $this->data['name']);
   }
 
   public function test_getId()
