655405e14a4f0b4b8e6b14ee370747dd9203f81c
Demiddel-Jesse <jesse.demiddel19@gmail.com>
2024-04-30 09:43:30 +0200

    finished functions

--- App/Data/ListTypeDAO.php
+++ App/Data/ListTypeDAO.php
@@ -33,4 +33,50 @@ class ListTypeDAO
   {
     $this->pdo->commit();
   }
+
+  public function getById(int $id): ?ListType
+  {
+    $sql = 'SELECT * FROM `ListTypes` WHERE `id` = :id';
+    $stmt = $this->pdo->prepare($sql);
+    $stmt->bindValue(':id', $id);
+    $stmt->execute();
+    $listTypeData = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if ($stmt->rowCount() > 0) {
+      $copy = new ListType($listTypeData['id'], $listTypeData['name']);
+      return $copy;
+    }
+    return null;
+  }
+
+  public function getByName(string $name): ?ListType
+  {
+    $sql = 'SELECT * FROM `ListTypes` WHERE `name` = :name';
+    $stmt = $this->pdo->prepare($sql);
+    $stmt->bindValue(':name', $name);
+    $stmt->execute();
+    $listTypeData = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if ($stmt->rowCount() > 0) {
+      $copy = new ListType($listTypeData['id'], $listTypeData['name']);
+      return $copy;
+    }
+    return null;
+  }
+
+  public function getAll(): array
+  {
+    $sql = 'SELECT * FROM `ListTypes` ORDER BY `name` ASC';
+    $stmt = $this->pdo->prepare($sql);
+    $stmt->execute();
+    $listTypesData = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    $listTypes = array();
+
+    foreach ($listTypesData as $listTypeData) {
+      array_push($listTypes, new ListType($listTypeData['id'], $listTypeData['name']));
+    }
+
+    return $listTypes;
+  }
 }
