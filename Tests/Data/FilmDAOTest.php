0408d28860848376b0a41c0539bea0a46f84a28b
Demiddel-Jesse <jesse.demiddel19@gmail.com>
2024-04-30 08:58:01 +0200

    expanded the tests a bit

--- Tests/Data/FilmDAOTest.php
+++ Tests/Data/FilmDAOTest.php
@@ -20,7 +20,8 @@ class FilmDAOTest extends TestCase
     $this->filmDAO = new filmDAO();
 
     $this->allObjects = [
-      new Film(1, 'The Death of Stalin', 1, 'In early-1953 Moscow, under the Great Terror\'s heavy cloak of state paranoia, the ever-watchful Soviet leader, Joseph Stalin, collapses unexpectedly of a brain haemorrhage. As a result, when someone discovers his body the following morning, a frenetic surge of raw panic starts spreading like a virus among the senior members of the Council of Ministers, as they scramble to maintain order, weed out the competition, and ultimately take power. But in the middle of a gut-wrenching rollercoaster of incessant plotting, tireless machinations, and frail allegiances, absolutely no one is safe; not even the feared chief of the secret police, Lavrenti Beria. In the end, who will prevail after the death of Stalin?', 107, new DateTime('2017-09-08'), null, 1, 1), new Film(2, 'Armageddon', 4, 'A massive meteor shower destroys the orbiting Space Shuttle Atlantis, before entering the atmosphere and bombarding New York City, Boston, Philadelphia, Moncton, Halifax, and Newfoundland. The meteors were pushed out of the asteroid belt by a collision from a rogue comet and a massive asteroid the size of Texas, and NASA learns that the asteroid will impact Earth in 18 days, potentially wiping out all life on Earth. NASA devises a plan to have a deep hole drilled into the asteroid, into which they will insert and detonate a nuclear bomb to destroy the asteroid. ', 150, new DateTime('1998-09-01'), '~/img/Armageddon.png', 4, 3)
+      new Film(2, 'Armageddon', 4, 'A massive meteor shower destroys the orbiting Space Shuttle Atlantis, before entering the atmosphere and bombarding New York City, Boston, Philadelphia, Moncton, Halifax, and Newfoundland. The meteors were pushed out of the asteroid belt by a collision from a rogue comet and a massive asteroid the size of Texas, and NASA learns that the asteroid will impact Earth in 18 days, potentially wiping out all life on Earth. NASA devises a plan to have a deep hole drilled into the asteroid, into which they will insert and detonate a nuclear bomb to destroy the asteroid. ', 150, new DateTime('1998-09-01'), '~/img/Armageddon.png', 4, 3),
+      new Film(1, 'The Death of Stalin', 1, 'In early-1953 Moscow, under the Great Terror\'s heavy cloak of state paranoia, the ever-watchful Soviet leader, Joseph Stalin, collapses unexpectedly of a brain haemorrhage. As a result, when someone discovers his body the following morning, a frenetic surge of raw panic starts spreading like a virus among the senior members of the Council of Ministers, as they scramble to maintain order, weed out the competition, and ultimately take power. But in the middle of a gut-wrenching rollercoaster of incessant plotting, tireless machinations, and frail allegiances, absolutely no one is safe; not even the feared chief of the secret police, Lavrenti Beria. In the end, who will prevail after the death of Stalin?', 107, new DateTime('2017-09-08'), null, 1, 1)
     ];
 
     $this->filmDAO->startTransaction();
@@ -72,9 +73,10 @@ class FilmDAOTest extends TestCase
     $this->assertEquals('Deathie stalinium', $this->filmDAO->getById(1)->getTitle());
     $this->assertEquals('~/img/stalinium.png', $this->filmDAO->getById(1)->getCoverImage());
 
-    $this->filmDAO->updateFilm(2, 'Deathie stalinium', 'Deathie stalinium', null, null, null, '~/img/stalinium.png', null, null, null);
-    $this->assertEquals('Deathie stalinium', $this->filmDAO->getById(2)->getTitle());
-    $this->assertEquals('~/img/stalinium.png', $this->filmDAO->getById(2)->getCoverImage());
+    $this->filmDAO->updateFilm(2, 'Deathie stalin', null, null, null, null, '~/img/stalium.png', 4, null, null);
+    $this->assertEquals('Deathie stalin', $this->filmDAO->getById(2)->getTitle());
+    $this->assertEquals('~/img/stalium.png', $this->filmDAO->getById(2)->getCoverImage());
+    $this->assertEquals(4, $this->filmDAO->getById(2)->getGenreId());
   }
 
   public function test_removeFilm()
