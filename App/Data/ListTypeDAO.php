ce4ef4df317353312a2bd8f092987971ea6fd0e2
Demiddel-Jesse <jesse.demiddel19@gmail.com>
2024-04-30 09:38:18 +0200

    created basic ListTypeDAO class

---
+++ App/Data/ListTypeDAO.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Data;
+
+use App\Entities\ListType;
+use App\Data\DBConfig;
+use PDO;
+
+class ListTypeDAO
+{
+  private PDO $pdo;
+
+  public function __construct()
+  {
+    $this->pdo = new PDO(
+      DBConfig::$DB_CONNSTRING,
+      DBConfig::$DB_USERNAME,
+      DBConfig::$DB_PASSWORD
+    );
+  }
+
+  public function startTransaction()
+  {
+    $this->pdo->beginTransaction();
+  }
+  public function rollbackTransaction()
+  {
+    $this->pdo->rollBack();
+  }
+  public function commitTransaction()
+  {
+    $this->pdo->commit();
+  }
+}
