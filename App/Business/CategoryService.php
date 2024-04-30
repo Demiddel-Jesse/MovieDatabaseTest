2c3785943f977f6c2090ce11e38f91d7a0506fd7
Demiddel-Jesse <jesse.demiddel19@gmail.com>
2024-04-30 13:22:11 +0200

    created categoryService

---
+++ App/Business/CategoryService.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Business;
+
+use App\Data\CategoryDAO;
+use App\Entities\Category;
+
+class CategoryService
+{
+  private $categoryDAO;
+
+  public function __construct()
+  {
+    $this->categoryDAO = new CategoryDAO();
+  }
+
+  public function getCategory(int|string $category): ?Category
+  {
+    if (is_int($category)) {
+      return $this->categoryDAO->getById($category);
+    } else if (is_string($category)) {
+      return $this->categoryDAO->getByName($category);
+    }
+    return null;
+  }
+
+  public function getAllCategories(): array
+  {
+    return $this->categoryDAO->getAll();
+  }
+
+  public function createNewCategory(string $name): void
+  {
+    $this->categoryDAO->createNewCategory($name);
+  }
+
+  public function updateCategory(string|int $category, string $newName): void
+  {
+    $this->categoryDAO->updateCategory($category, $newName);
+  }
+
+  public function removeCategory(int|string $category): void
+  {
+    $this->categoryDAO->removeCategory($category);
+  }
+}
