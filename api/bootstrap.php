dd950ab4df435dc4ddb422790857f0804eec7969
Demiddel-Jesse <jesse.demiddel19@gmail.com>
2024-07-09 13:53:03 +0200

    small error

--- api/bootstrap.php
+++ api/bootstrap.php
@@ -15,5 +15,5 @@ require_once __DIR__ . "/../vendor/autoload.php";
 use Twig\Loader\FilesystemLoader;
 use Twig\Environment;
 
-$loader = new FilesystemLoader(__DIR__ . 'Presentation');
+$loader = new FilesystemLoader(__DIR__ . '/Presentation');
 $twig = new Environment($loader);
