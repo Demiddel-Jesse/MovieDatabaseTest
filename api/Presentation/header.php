96d13c0ecc925cd729bbd096e9bfd577e1cf0a46
Demiddel-Jesse <jesse.demiddel19@gmail.com>
2024-07-10 16:29:34 +0200

    another attempt

--- api/Presentation/header.php
+++ api/Presentation/header.php
@@ -12,7 +12,25 @@
   <script src="./api/js/script.js" defer></script>
   <link rel="stylesheet" href="./api/css/index.css">
 </head>
+<script>
+  const fs = require('fs');
+const path = require('path');
+const directory = './';
 
+// use readdir method to read the files of the direcoty 
+fs.readdir(directory, (err, files) => {
+  files.forEach(file => {
+    // get the details of the file 
+    let fileDetails = fs.lstatSync(path.resolve(directory, file));
+    // check if the file is directory 
+    if (fileDetails.isDirectory()) {
+      console.log('Directory: ' + file);
+    } else {
+      console.log('File: ' + file);
+    }
+  });
+});
+</script>
 <body>
   <nav class="c-nav">
     <ul class="c-nav__standard">
