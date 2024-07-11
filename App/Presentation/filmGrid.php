d7fe851e5036c884f7c1fc3a493c2a7685ed4a61
Demiddel-Jesse <jesse.demiddel19@gmail.com>
2024-07-11 12:12:28 +0200

    pls

--- App/Presentation/filmGrid.php
+++ App/Presentation/filmGrid.php
@@ -12,7 +12,7 @@
 <section class="c-filmGrid">
   <?php
   foreach ($films as $film) {
-    $film->setCoverImage(str_replace('~', './App', $film->getCoverImage()));
+    $film->setCoverImage(str_replace('~', __DIR__ .'/App', $film->getCoverImage()));
     $averageRating = $filmService->getAverageRating($film->getId());
 
     echo $twig->render('filmButton.twig', array('film' => $film, 'rating' => $averageRating));
