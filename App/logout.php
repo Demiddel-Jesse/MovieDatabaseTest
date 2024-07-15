4418de5f2cdfe56d789eebb14ebafe480776ffe1
Demiddel-Jesse <jesse.demiddel19@gmail.com>
2024-07-15 09:21:51 +0200

    test

--- App/logout.php
+++ App/logout.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 session_start();
 
-unset($_COOKIE["admin"]);
-unset($_COOKIE["user"]);
+// unset($_COOKIE["admin"]);
+// unset($_COOKIE["user"]);
+setcookie("admin", "", time()-3600);
+setcookie("user", "", time()-3600);
 header('location: index.php');
 exit(0);
